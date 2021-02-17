beb3efd43cd63612f1b360e2d8c6df141f8c32b4
aqmery <henrydelange8412@gmail.com>
2021-02-17 16:01:00 +0100

    updated Formatieve_opdracht_2a
    added better spacing and comments to the Formatieve opdracht 2a

--- Formatieve_opdracht_2a.py
+++ Formatieve_opdracht_2a.py
@@ -8,18 +8,21 @@ for collectionName in collectionsNames:
     #print(f'Collection {collectionName} contains {collection.estimated_document_count()} documents')
 
 
-
+"""shows the first document in the Mongodb database"""
 products = MongodbDAO.getDocuments('products')
 # products is een cursor
-print(f'first document = {products.next()["name"]} price: {products.next()["price"]["selling_price"]}')
+print(f'the first document in the database : {products.next()["name"]} price: {products.next()["price"]["selling_price"]}')
 
 
+"""shows the first product starting with the letter "R" in de database"""
 products = MongodbDAO.getDocuments("products")
 for product in products:
     if product['name'][0] == 'R':
-        print(product['name'])
+        print(f'the first product in the list starting with the letter "R" : {product["name"]}')
         break
 
+
+"""calculates the avrage price of all the items in the database in cents"""
 products = MongodbDAO.getDocuments("products")
 total = 0
 for product in products:
@@ -27,5 +30,4 @@ for product in products:
         total += product["price"]["selling_price"]
     except:
         continue
-
-print(f"average price of the database in cents : {round(total/db.get_collection('products').estimated_document_count())}")
+print(f"average price of all the items in the database in cents : {round(total/db.get_collection('products').estimated_document_count())}")
