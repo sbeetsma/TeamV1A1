de95734870ef6915ac1589094940d002f18c2bd3
TheOGZer0 <maartenjeu@gmail.com>
2021-03-04 16:13:40 +0100

    Added construct_insert_query()

--- mongo_to_pg.py
+++ mongo_to_pg.py
@@ -11,8 +11,26 @@ def rfd(dict: dict, key):
 
     Returns:
         if the key exists in the dictionairy, returns the value associated with the key.
-        if it doesn't exist, returns none"""
+        if it doesn't exist, returns None"""
     try:
         return dict[key]
     except KeyError:
         return None
+
+
+def construct_insert_query(table_name: str, var_names: list[str]) -> str:
+    """Constructs an SQL insert query, formatted to use %s in place of actual values to allow PsycoPG2 to properly format them.
+
+    Args:
+        table_name: the name of the table to insert to insert into.
+        var_names: a list containing all the attributes to insert.
+
+    Returns:
+        A properly formatted SQL query as string.
+    """
+    q = f"INSERT INTO {table_name} ("
+    q += var_names[0]
+    for var in var_names[1:]:
+        q += ", " + var
+    q += f") VALUES (%s{(len(var_names) - 1) * ', %s'});"
+    return q
