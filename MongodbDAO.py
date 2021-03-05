fd55ca52f1accf4d9085dcb54b3f5ab70042d433
TheOGZer0 <maartenjeu@gmail.com>
2021-03-05 17:20:12 +0100

    Made simple_mongo_to_db() autoconvert dumb datatypes to strings

--- MongodbDAO.py
+++ MongodbDAO.py
@@ -24,3 +24,35 @@ def getDocuments(collectionName, filter = {}):
     Returns de gefilterde collectie als object
         """
     return getCollection(collectionName).find(filter)
+###EXPIRIMENTAL
+
+def retrieve_from_dict(dict: dict, key):
+    """Tries to retrieve a value from a dictionairy with a certain key, and catches the KeyError if it doesn't exist.
+
+    Args:
+        dict: the dictionairy to retrieve the value from.
+        key: the key associated with the desired data.
+
+    Returns:
+        if the key exists in the dictionairy, returns the value associated with the key.
+        if it doesn't exist, returns None"""
+    try:
+        return dict[key]
+    except KeyError:
+        return None
+
+
+
+
+
+c = getDocuments("sessions")
+
+
+for sesh in c:
+    i = retrieve_from_dict(sesh, '_id')
+    s = retrieve_from_dict(sesh, 'segment')
+    if (not isinstance(i, str)) and i != None:
+        aangepast = str(i)
+        print(aangepast)
+    if (not isinstance(s, str)) and s != None:
+        print(f"segment:{s}\n")
